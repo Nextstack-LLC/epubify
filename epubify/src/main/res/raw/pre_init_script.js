57441bdcff4f4126e16042e4acb56753d651cb94
gurgen.khachatryan <gurgen.khachatryan@nextstack.org>
2024-06-12 15:25:13 +0400

    - Fixed inner padding

--- epubify/src/main/res/raw/pre_init_script.js
+++ epubify/src/main/res/raw/pre_init_script.js
@@ -1,9 +1,9 @@
 function setBodyPadding(topDp, bottomDp, rightDp, leftDp) {
     var body = document.getElementsByTagName('body')[0];
-    body.style.marginLeft = window.devicePixelRatio * leftDp + 'px';
-    body.style.marginRight = window.devicePixelRatio * rightDp + 'px';
-    body.style.marginTop = window.devicePixelRatio * topDp + 'px';
-    body.style.marginBottom = window.devicePixelRatio * bottomDp + 'px';
+    body.style.paddingLeft = (window.devicePixelRatio * leftDp) + 'px';
+    body.style.paddingRight = (window.devicePixelRatio * rightDp) + 'px';
+    body.style.paddingTop = (window.devicePixelRatio * topDp) + 'px';
+    body.style.paddingBottom = (window.devicePixelRatio * bottomDp) + 'px';
 }
 
 // Remove all padding and margin from the body
